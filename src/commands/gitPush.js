2f0bdbeedb13e42b5c9f7e633d26af4d24d7a92e
Leo Gutierrez <lgutierrez@griddynamics.com>
2022-12-12 13:58:48 -0600

    feat: :sparkles: #3 - Added support to push to origin rigth after commit

---
+++ src/commands/gitPush.js
@@ -0,0 +1,17 @@
+// @flow
+import { execaSync } from 'execa'
+
+const gitPush = async (): Promise<void> => {
+    try {
+        let { stdout } = execaSync('git', ['push', 'origin', retrieveCurrentBranchName()])
+        console.log('\n'+stdout)
+    } catch(error) {
+        return Promise.reject(error.message)
+    }
+}
+
+const retrieveCurrentBranchName = (): string => {
+    return execaSync('git', ['rev-parse', '--abbrev-ref', 'HEAD']).stdout
+}
+
+export default gitPush
